f67b9cbbfd96324e871552b40506343707a9ab63
Al J Abadi <al.jal.abadi@gmail.com>
2020-02-21 10:27:34 +1100

    add nipals no gram-schmidt benchmark

--- src/utils/utils.R
+++ src/utils/utils.R
@@ -181,3 +181,12 @@ dcast_dt_list <-
                as.matrix()
            })
   }
+
+## ----------- rbindListWithNames ----------- 
+rbindListWithNames <- function(lst, new_col = "dataset") {
+  lst_with_newcol <- mapply(x=names(lst), y=lst, FUN = function(x, y){
+    y[,new_col] <- x
+    y
+  }, SIMPLIFY = FALSE)
+  Reduce(rbind, lst_with_newcol)
+}
