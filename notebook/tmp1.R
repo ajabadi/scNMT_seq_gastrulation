485d0ee2e6389362ddbe9634185d491702a55cee
Al J Abadi <al.jal.abadi@gmail.com>
2020-06-18 00:45:57 +1000

    remove local source files

--- notebook/tmp1.R
+++
@@ -1,8 +0,0 @@
-library(MultiAssayExperiment)
-library(here)
-library(ggplot2)
-library(knitr)
-
-
-mmsplsda_E7.5 <- multimodal_analysis_wrapper(mae = gastru.mae, study_assays = NULL, ncomp = 3, scale = FALSE, design = 'null', lineages = NULL, stages = "E7.5", DA = 'stage_lineage', keepX = NULL, save = FALSE)
-saveRDS(mmspls, file = 'savedata/MultiModalSparsePLSDA-E7.5.rds')
--- notebook/tmp2.R
+++
@@ -1,7 +0,0 @@
-library(MultiAssayExperiment)
-library(here)
-library(ggplot2)
-library(knitr)
-
-mmspls_E7.5 <- multimodal_analysis_wrapper(mae = gastru.mae, study_assays = NULL, ncomp = 3, scale = FALSE, design = 'null', lineages = NULL, stages = "E7.5", DA = NULL, keepX = NULL, save = FALSE)
-saveRDS(mmspls, file = 'savedata/MultiModalSparsePLS-E7.5.rds')
